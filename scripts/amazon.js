0683bbd9006d0a97c65de14258987023a006dfc3
Neng <xuneng1998@gmail.com>
2025-03-03 22:29:14 +0800

    enable quantity numbers

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class = 'js-quantity-selector-${product.id}'>
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -70,13 +70,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
             }
         });
 
+        const Quantity_increment = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
         if (matchingItem) {
-            matchingItem.quantity++;
+            matchingItem.quantity+= Quantity_increment;
         }
         else{
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: Quantity_increment
             })
         }
 
@@ -87,7 +89,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
     });
 
     document.querySelector('.js-cart-quantity').textContent = cartCount;
-
 
     });
 });
